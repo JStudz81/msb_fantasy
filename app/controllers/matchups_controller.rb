b0d9169f3cdafdf03ab4763346cb6453309f39fc
Jstudz81 <jack.studzinski@gmail.com>
2023-01-29 11:51:49 -0500

    matchup points totals and formatting

--- app/controllers/matchups_controller.rb
+++ app/controllers/matchups_controller.rb
@@ -1,6 +1,12 @@
 class MatchupsController < ApplicationController
+    include ActionView::Helpers::NumberHelper
+
     def show
         @matchups = Matchup.where(week_num: params[:weekNum])
+        @week_num = params[:weekNum]
         @weeks = Matchup.all.distinct.pluck("week_num")
+
+
+    
     end
 end
